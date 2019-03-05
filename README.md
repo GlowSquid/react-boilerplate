19a2a9f60e3b2bdff8d5a7e3b32b9895a7fa0617
glowsquid <koz@glowsquid.com>
2019-03-05 10:31:01 +0100

    finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React Boilerplate
+
+**Babel-Webpack-React starter template.**
+
+ESlint and LESS already integrated
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="logo">React Boilerplate</h1>
+      <h1 className="logo">
+        React <span>Boilerplate</span>
+      </h1>
     </div>
   );
 };
